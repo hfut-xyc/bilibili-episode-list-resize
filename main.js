318e706ed1266a518f9283466766b4f95b530d3a
hfut-xyc <hfut.xieyincen@foxmail.com>
2025-07-16 23:46:07 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         自定义设置B站的视频选集列表高度
+// @namespace    http://tampermonkey.net/
+// @version      2025-07-13
+// @description  try to take over the world!
+// @author       You
+// @icon         https://www.bilibili.com/favicon.ico
+// @match        https://space.bilibili.com/*
+// @match        https://www.bilibili.com/video/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+    window.addEventListener('load', function() {
+        let x = document.querySelector('div.video-pod__body');
+        console.log(x);
+        if (x !== null) {
+            x.style.maxHeight = '500px';
+        }
+    });
+})();
