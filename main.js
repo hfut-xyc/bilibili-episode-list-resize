dfc6c68d28736ac8622b11bc7558af774623a181
hfut-xyc <hfut.xieyincen@foxmail.com>
2025-07-17 21:32:04 +0800

    删除广告

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         自定义设置B站的视频选集列表高度
+// @name         B站视频选集自定义高度、去除广告
 // @namespace    http://tampermonkey.net/
 // @version      2025-07-13
 // @description  try to take over the world!
@@ -14,18 +14,46 @@
     'use strict';
     // Your code here...
     window.addEventListener('load', function() {
-        // 视频投稿
+        // 设置视频页面-选集列表的最大高度
         let x = document.querySelector('div.video-pod__body');
         console.log(x);
         if (x !== null) {
             x.style.maxHeight = '500px';
         }
 
-        // 番剧
+        // 设置番剧页面-选集列表的最大高度
         let y = document.querySelector('div.imageList_wrap___f73Z');
         console.log(y);
         if (y !== null) {
             y.style.maxHeight = '500px';
         }
+
+        // 删除视频选集上方广告
+        let ad1 = document.querySelector('div.video-card-ad-small');
+        console.log(ad1);
+        if (ad1 !== null) {
+           ad1.remove();
+        }
+
+        // 删除评论区上方活动
+        // let act = document.querySelector('div.activity-m-v1');
+        // console.log(act);
+        // if (act !== null) {
+        //    act.remove();
+        // }
+
+        // // 删除评论区上方广告
+        // let ad2 = document.querySelector('div.ad-report.ad-floor-exp.left-banner');
+        // console.log(ad2);
+        // if (ad2 !== null) {
+        //    ad2.remove();
+        // }
+
+        // // 删除页面最右下角广告
+        // let ad3 = document.querySelector('div.ad-report.ad-floor-exp.right-bottom-banner');
+        // console.log(ad3);
+        // if (ad3 !== null) {
+        //    ad3.remove();
+        // }
     });
 })();
